4e3408fc18aaa25573ec41b7dd5de2022e0caeac
Rown-alt <79279060+Rown-alt@users.noreply.github.com>
2022-05-16 17:22:20 +0300

    Delete Repository

--- app/src/main/java/com/example/movieapp/api/Repository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.movieapp.api
-
-import com.example.movieapp.models.MovieList
-
-class Repository {
-    suspend fun getPremieres(month : String) : MovieList{
-        return RetrofitInstance.api.getPremieres(month)
-    }
-}

8c8daf41a82f42e309c877d207cdf0413f68025c
Rown-alt <79279060+Rown-alt@users.noreply.github.com>
2022-05-21 15:30:01 +0300

    fixed Api interface

--- app/src/main/java/com/example/movieapp/api/KinoPoiskAPI.kt
+++ app/src/main/java/com/example/movieapp/api/KinoPoiskAPI.kt
@@ -1,18 +1,22 @@
 package com.example.movieapp.api
 
+import com.example.movieapp.models.actorById.ActorById
 import com.example.movieapp.models.MovieById
 import com.example.movieapp.models.MovieList
-import com.example.movieapp.models.Staff
+import com.example.movieapp.models.Person
 import retrofit2.http.*
 
 interface KinoPoiskAPI {
     @Headers("X-API-KEY: 9dea54ec-4608-424b-98a4-3cd3b76af31e")
-    @GET("v2.2/films/premieres?year=2001")
-    suspend fun getPremieres(@Query("month") month : String) : MovieList
+    @GET("v2.2/films/premieres?year=2002")
+    suspend fun getPremieres(@Query("year") year : Int ,@Query("month") month : String) : MovieList
     @Headers("X-API-KEY: 9dea54ec-4608-424b-98a4-3cd3b76af31e")
     @GET("v2.2/films/{id}")
     suspend fun getMovie(@Path("id") id : Int) : MovieById
     @Headers("X-API-KEY: 9dea54ec-4608-424b-98a4-3cd3b76af31e")
     @GET("v1/staff")
-    suspend fun  getStaff(@Query("filmId") movieID : Int) : Staff
+    suspend fun  getStaff(@Query("filmId") movieID : Int) : List<Person>
+    @Headers("X-API-KEY: 9dea54ec-4608-424b-98a4-3cd3b76af31e")
+    @GET("v1/staff/{id}")
+    suspend fun getActorById(@Path("id") actorId : Int) : ActorById
 }

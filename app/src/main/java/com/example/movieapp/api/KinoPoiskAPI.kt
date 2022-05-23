681155fef27a7edd89e8c58f3de78c020e35f05f
Rown-alt <79279060+Rown-alt@users.noreply.github.com>
2022-05-23 23:02:12 +0300

    Request for tops commit

--- app/src/main/java/com/example/movieapp/api/KinoPoiskAPI.kt
+++ app/src/main/java/com/example/movieapp/api/KinoPoiskAPI.kt
@@ -4,11 +4,12 @@ import com.example.movieapp.models.actorById.ActorById
 import com.example.movieapp.models.MovieById
 import com.example.movieapp.models.MovieList
 import com.example.movieapp.models.Person
+import com.example.movieapp.models.TopOfFilms
 import retrofit2.http.*
 
 interface KinoPoiskAPI {
     @Headers("X-API-KEY: 9dea54ec-4608-424b-98a4-3cd3b76af31e")
-    @GET("v2.2/films/premieres?year=2002")
+    @GET("v2.2/films/premieres")
     suspend fun getPremieres(@Query("year") year : Int ,@Query("month") month : String) : Result<MovieList>
     @Headers("X-API-KEY: 9dea54ec-4608-424b-98a4-3cd3b76af31e")
     @GET("v2.2/films/{id}")
@@ -19,4 +20,7 @@ interface KinoPoiskAPI {
     @Headers("X-API-KEY: 9dea54ec-4608-424b-98a4-3cd3b76af31e")
     @GET("v1/staff/{id}")
     suspend fun getActorById(@Path("id") actorId : Int) : Result<ActorById>
+    @Headers("X-API-KEY: 9dea54ec-4608-424b-98a4-3cd3b76af31e")
+    @GET("v2.2/films/top")
+    suspend fun getTop(@Query("type") type : Int, @Query("page") page : Int) : Result<TopOfFilms>
 }

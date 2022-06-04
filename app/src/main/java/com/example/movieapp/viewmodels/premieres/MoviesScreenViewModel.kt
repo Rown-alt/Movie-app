8f5be9b6ae28e278714bd83162c65111f2b71cd1
Rown-alt <79279060+Rown-alt@users.noreply.github.com>
2022-06-05 00:56:15 +0300

    Functions for top of films

--- app/src/main/java/com/example/movieapp/viewmodels/premieres/MoviesScreenViewModel.kt
+++ app/src/main/java/com/example/movieapp/viewmodels/premieres/MoviesScreenViewModel.kt
@@ -1,27 +1,61 @@
 package com.example.movieapp.viewmodels.premieres
 
+
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.movieapp.R
 import com.example.movieapp.api.RetrofitInstance
-import com.example.movieapp.fragments.ErrorFragment
 import com.example.movieapp.models.Movie
+import com.example.movieapp.models.top_of_films.FilmsTop
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class MoviesScreenViewModel : ViewModel() {
+
     var movies = MutableLiveData<ArrayList<Movie>>()
+
+    var top = MutableLiveData<ArrayList<FilmsTop>>()
+    private var topArray = ArrayList<FilmsTop>()
+    private var randomNum = Random.nextInt(0, 10)
+
     var exception = MutableLiveData<String>()
+
     fun getPremieres(year : Int,month : String){
        viewModelScope.launch {
            RetrofitInstance.api.getPremieres(year,month).onSuccess {
                movies.value = it.items
            }
            RetrofitInstance.api.getPremieres(year,month).onFailure {
-               exception.value = it.toString()
+               exception.value = it.localizedMessage
            }
        }
+    }
+
+    private fun addTop(type : String, page : Int)
+    {
+        viewModelScope.launch {
+            RetrofitInstance.api.getTop(type, page).onSuccess {
+                Log.e("Request", "Request sent")
+                val randomPage = Random.nextInt(1, it.pagesCount)
+                RetrofitInstance.api.getTop(type, randomPage).onSuccess {
+                    for (i in randomNum..randomNum+9){
+                        topArray.add(it.films[i])
+                    }
+                }
+                RetrofitInstance.api.getTop(type, randomPage).onFailure {
+                    exception.value = it.localizedMessage
+                }
+            }
+            RetrofitInstance.api.getTop(type, page).onFailure {
+                exception.value = it.localizedMessage
+            }
+        }
+    }
+    fun getTop(type : String ){
+        viewModelScope.launch {
+           addTop(type, 1)
+           top.value = topArray
+        }
     }
 }

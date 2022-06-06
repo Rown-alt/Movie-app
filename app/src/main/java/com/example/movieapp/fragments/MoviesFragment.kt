8c9936abab270fa03d38c529356ff7882d6f59c7
Rown-alt <79279060+Rown-alt@users.noreply.github.com>
2022-06-06 17:40:39 +0300

    Added random film on main screen

--- app/src/main/java/com/example/movieapp/fragments/MoviesFragment.kt
+++ app/src/main/java/com/example/movieapp/fragments/MoviesFragment.kt
@@ -1,14 +1,18 @@
 package com.example.movieapp.fragments
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movieapp.R
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.bumptech.glide.Glide
 import com.example.movieapp.adapter.PremieresAdapter
 import com.example.movieapp.adapter.MovieByIdAdapter
 import com.example.movieapp.adapter.TopAdapter
@@ -27,14 +31,22 @@ class MoviesFragment : Fragment(R.layout.movies_screen){
 
     private lateinit var recyclerViewTop : RecyclerView
     private var topAdapter = TopAdapter()
+
+    private lateinit var randomFilmIV : ImageView
+    private lateinit var randomFilmName : TextView
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         recyclerViewPremieres = view.findViewById(R.id.moviesRV)
         recyclerViewSeries = view.findViewById(R.id.seriesRV)
         recyclerViewTop = view.findViewById(R.id.topRV)
+
+        randomFilmName = view.findViewById(R.id.randomFilmName)
+        randomFilmIV = view.findViewById(R.id.randomFilmIV)
+        var randomFilmId = 100000
         viewModelPremieres.exception.observe(viewLifecycleOwner){
             if (it!=null){
-                var bundle = Bundle()
+                val bundle = Bundle()
                 bundle.putString("exceptionName", it.toString())
                 bundle.putString("fragmentName", "MoviesFragment")
                 val errorFragment = ErrorFragment()
@@ -54,7 +66,16 @@ class MoviesFragment : Fragment(R.layout.movies_screen){
         viewModelSeries.series.observe(viewLifecycleOwner){
             seriesAdapter.setSeries(it)
         }
-
+        viewModelPremieres.randomFilm.observe(viewLifecycleOwner){
+            randomFilmName.text = it.nameRu
+            Glide.with(view).load(it.posterUrl).into(randomFilmIV)
+            randomFilmId = it.filmId
+        }
+        randomFilmIV.setOnClickListener {
+            val action = MoviesFragmentDirections.actionMoviesFragmentToDetailsFragment(randomFilmId)
+            it.findNavController().navigate(action)
+            Log.e("AAA", "Data sent")
+        }
         recyclerViewPremieres.adapter = movieAdapter
         recyclerViewPremieres.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL)
 
@@ -67,8 +88,10 @@ class MoviesFragment : Fragment(R.layout.movies_screen){
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
+
         viewModelPremieres.getPremieres(Random.nextInt(1995, 2021),"MAY")
         viewModelPremieres.getTop("TOP_250_BEST_FILMS")
         viewModelSeries.getSeries()
+        viewModelPremieres.getRandomFilm()
     }
 }

ac2fd09bda7518765795a655a0bdb8a47e4e972b
Rown-alt <79279060+Rown-alt@users.noreply.github.com>
2022-06-07 23:31:58 +0300

    Function to get similar films

--- app/src/main/java/com/example/movieapp/viewmodels/detailsFragment/DetailsFragmentViewModel.kt
+++ app/src/main/java/com/example/movieapp/viewmodels/detailsFragment/DetailsFragmentViewModel.kt
@@ -5,17 +5,31 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.movieapp.api.RetrofitInstance
 import com.example.movieapp.models.MovieById
+import com.example.movieapp.models.similars.Similar
 import kotlinx.coroutines.launch
 
 class DetailsFragmentViewModel() : ViewModel() {
     var movieById = MutableLiveData<MovieById>()
+    var similarFilms = MutableLiveData<ArrayList<Similar>>()
     var exception = MutableLiveData<String>()
+
     fun getMovieById(id : Int) {
         viewModelScope.launch {
             RetrofitInstance.api.getMovie(id).onSuccess {
                 movieById.value = it
             }
             RetrofitInstance.api.getMovie(id).onFailure {
+                exception.value = it.localizedMessage
+            }
+        }
+    }
+
+    fun getSimilars(id : Int){
+        viewModelScope.launch {
+            RetrofitInstance.api.getSimilars(id).onSuccess {
+                similarFilms.value = it.items
+            }
+            RetrofitInstance.api.getSimilars(id).onFailure {
                 exception.value = it.localizedMessage
             }
         }

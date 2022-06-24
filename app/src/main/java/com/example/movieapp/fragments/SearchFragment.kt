7be92cd63618ce18d4427d1c0c9ecb3aad7e4aad
Rown-alt <79279060+Rown-alt@users.noreply.github.com>
2022-06-24 18:16:36 +0300

    Search fragment

---
+++ app/src/main/java/com/example/movieapp/fragments/SearchFragment.kt
@@ -0,0 +1,30 @@
+package com.example.movieapp.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+
+class SearchFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SearchFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.search_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+//        viewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
+//        // TODO: Use the ViewModel
+    }
+
+}

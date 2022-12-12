a8795950163676f44799bfc3d1345a1973c658f6
Rown-alt <79279060+Rown-alt@users.noreply.github.com>
2022-12-12 21:59:19 +0300

    Datamodule update

--- app/src/main/java/com/example/movieapp/di/DataModule.kt
+++ app/src/main/java/com/example/movieapp/di/DataModule.kt
@@ -2,13 +2,7 @@ package com.example.movieapp.di
 
 import androidx.room.Room
 import com.example.data.AppDatabase
-import com.example.movieapp.AppApplication
-import com.example.movieapp.fragments.FavouritesFragment
-import com.example.movieapp.fragments.abstractions.FilmDetailsAbstractFragment
-import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.fragment.dsl.fragment
-import org.koin.dsl.koinApplication
 import org.koin.dsl.module
 
 val dataModule = module{

3df7b312b0fb46485759223f780ba14911bc2ee4
Rown-alt <79279060+Rown-alt@users.noreply.github.com>
2022-12-16 23:14:32 +0300

    SearchViewModel update

--- app/src/main/java/com/example/movieapp/viewmodels/searchFragment/SearchViewModel.kt
+++ app/src/main/java/com/example/movieapp/viewmodels/searchFragment/SearchViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.movieapp.models.FiltersRequest
 import com.example.movieapp.models.Movie
 import com.example.movieapp.models.films_by_keyword.FilmByKeyword
 import com.example.movieapp.repository.Repository
@@ -12,6 +13,7 @@ import kotlinx.coroutines.launch
 class SearchViewModel(private val repository: Repository) : ViewModel() {
     val movies = MutableLiveData<ArrayList<FilmByKeyword>>()
     var exception = MutableLiveData<String>()
+    var filters = MutableLiveData<FiltersRequest>()
 
     fun search(keyword: String){
         viewModelScope.launch{
@@ -27,6 +29,21 @@ class SearchViewModel(private val repository: Repository) : ViewModel() {
             }
             catch (exc: Exception){}
 
+        }
+    }
+
+    fun getFilters(){
+        viewModelScope.launch {
+            try{
+                val request = repository.getFilters()
+                request.onSuccess {
+                    filters.value = it
+                }
+                request.onFailure { msg->
+                    exception.value = msg.localizedMessage
+                }
+            }
+            catch (exc: Exception){}
         }
     }
 }
